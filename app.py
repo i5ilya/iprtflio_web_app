9143f315066ec370fcd19d202753021c7f8647e6
Ilya <ilya.ilyin5@gmail.com>
2022-06-28 10:07:21 +0300

    public first app (the age)

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+
+from the_age_app import the_age_app
+
 
 app = Flask(__name__)
 
@@ -11,6 +14,19 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@app.route("/app_the_age", methods=["GET", "POST"])
+def app_the_age():
+    if request.method == "POST":
+        age_app_name = request.form['age_app_name']
+        age_app_age = int(request.form['age_app_age'])
+        ansver = the_age_app(age_app_name, age_app_age)
+
+        return render_template('apps/app_the_age/app_the_age.html', age_app_name=age_app_name, age_app_age=age_app_age, ansver=ansver)
+
+    else:
+        return render_template('apps/app_the_age/app_the_age.html')
 
 
 if __name__ == '__main__':
