824ff532ecd57e798f8a280eda8a64df20813ece
Ilya <ilya.ilyin5@gmail.com>
2022-08-28 15:19:30 +0300

    finish app_words

--- app_sort_count_words.py
+++ app_sort_count_words.py
@@ -7,9 +7,9 @@ def count_sort_words(any_text):
     # Удалить знаки переноса строк, которые в Python пишется как '\n' и '\r'
     any_text = any_text.replace('\r', ' ')
 
-    punctuation = ['.', ',', ';', ':', '!', '?', '"',
-                       "'", '(', ')', '...', '—', '«', '»', '.', ' - ']
-    # punctuation = string.punctuation
+    # punctuation = ['.', ',', ';', ':', '!', '?', '"',
+    #                   "'", '(', ')', '...', '—', '«', '»', '.', ' - ', '`']
+    punctuation = string.punctuation
 
     # посчитаем знаки препинания
     def count_punctuation(some_string: str) -> str:
@@ -22,7 +22,7 @@ def count_sort_words(any_text):
     sum_punctuation = count_punctuation(any_text)
 
     # Удалим знаки препинания
-    def marks_remove(some_string: str) -> str:  
+    def marks_remove(some_string: str) -> str:
         return "".join(elements for elements in some_string if elements not in punctuation)
 
     # создадим новую строку уже без знаков препинания и сделаем все слова строчными
@@ -45,7 +45,7 @@ def count_sort_words(any_text):
 
     sorted_dict = {}  # создадим новый словарь
     ''' Сортировка:  Тут с помощью параметра "key" - указываем как именно осуществлять сортировку
-    И мы сортируем по значениям (метод .get возвращает значение ключа). Тоесть, значение - у нас кол-во слов,
+    И мы сортируем по значениям (метод .get возвращает значение ключа). То есть, значение - у нас кол-во слов,
     по нему и сортируем.  reverse - перевернуть'''
     sorted_keys = sorted(raw_dic, key=raw_dic.get, reverse=True)
 
@@ -54,35 +54,35 @@ def count_sort_words(any_text):
         sorted_dict[k] = raw_dic[k]
 
     list_one_word = []  # это под список слов, повторяющихся один раз.
-    dic_for_return = {}
+    dic_for_return = {}  # конечный словарь
     for key, value in sorted_dict.items():
         if value > 1:
-            #print(f'Слово "{key}" : повторений: {value}')
+            # print(f'Слово "{key}" : повторений: {value}')
             dic_for_return[key] = value
         if value == 1:
             list_one_word.append(key)  # добавим в цикле в список
+
+    list_one_word.sort()
     list_one_word = ", ".join(list_one_word)
 
-    # bild like a json)
+    # bild like a json data
     new_list = []
-
     for key, value in dic_for_return.items():
         new_dic = {}
         new_dic['word'] = key
         new_dic['sum'] = value
         new_list.append(new_dic)
 
-
     json_return = {'sum_all_words': len(mas),
                    'sum_unique_words': len(unique),
-                   'sum_punctuation' : sum_punctuation,
+                   'sum_punctuation': sum_punctuation,
                    'list_one_word': list_one_word,
-                  # 'dic_for_return': dic_for_return,
                    'new_list': new_list
                    }
     return json_return
 
 
 if __name__ == "__main__":
-    print(count_sort_words('This is the house that Jack built. This is the malt that lay in the house that Jack built. This is the rat that ate the malt That lay in the house that Jack built.'))
+    print(count_sort_words(
+        'This is the house that Jack built. This is the malt that lay in the house that Jack built. This is the rat that ate the malt That lay in the house that Jack built.'))
     print()
